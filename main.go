1893ed49d850c06fc245bed605e1dd9a9bccf94d
Savvas <aut153467@autom.teithe.gr>
2024-11-10 21:32:58 +0200

    fcking GOLAND

--- main.go
+++ main.go
@@ -23,9 +23,10 @@ var albums = []album{
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/snippet/view/{id}", snippetView) // Add the {id} wildcard segment
-	mux.HandleFunc("/snippet/create", snippetCreate)
+
 	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/snippet/view/{id}", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
@@ -37,17 +38,12 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	// Extract the value of the id wildcard from the request using r.PathValue()
-	// and try to convert it to an integer using the strconv.Atoi() function. If
-	// it can't be converted to an integer, or the value is less than 1, we
-	// return a 404 page not found response.
+
 	id, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
 		return
 	}
-	// Use the fmt.Sprintf() function to interpolate the id value with a
-	// message, then write it as the HTTP response.
 	msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
 	w.Write([]byte(msg))
 }
